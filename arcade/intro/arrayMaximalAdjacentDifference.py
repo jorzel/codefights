38f88732a41870f65ad5bd2293b3f9ed7dc282d0
jorzel1988 <jaroslaw.orzel@emplocity.pl>
2017-11-15 12:07:51 +0100

    intro level 5

---
+++ arcade/intro/arrayMaximalAdjacentDifference.py
@@ -0,0 +1,19 @@
+"""
+Given an array of integers, find the maximal absolute difference between any two of its adjacent elements.
+
+Example
+
+For inputArray = [2, 4, 1, 0], the output should be
+arrayMaximalAdjacentDifference(inputArray) = 3.
+"""
+
+
+def arrayMaximalAdjacentDifference(inputArray):
+    max_diff = 0
+    for i, el in enumerate(inputArray):
+        if i == 0:
+            continue
+        diff = abs(inputArray[i] - inputArray[i-1])
+        if diff > max_diff:
+            max_diff = diff
+    return max_diff
