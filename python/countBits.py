8d60b6111f1f2e4248e5c76cbc32cc85f59b11a1
jorzel <jaroslaw.orzel@emplocity.pl>
2018-07-04 21:31:53 +0200

    some python tasks

---
+++ python/countBits.py
@@ -0,0 +1,15 @@
+"""
+Implement a function that, given an integer n, uses a specific method on it and returns the number of bits in its binary representation.
+
+Note: in this task and most of the following tasks you will be given a code snippet with some part of it replaced by the ellipsis (...). Only this part is allowed to be changed.
+
+Example
+
+For n = 50, the output should be
+countBits(n) = 6.
+
+5010 = 1100102, a number that consists of 6 digits. Thus, the output should be 6.
+"""
+
+def countBits(n):
+    return n.bit_length()
